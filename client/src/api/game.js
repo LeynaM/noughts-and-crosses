df30551c774ec8c520cfe6fa61efe42037d85e62
LeynaM <leynamay@gmail.com>
2026-08-01 12:54:56 +0200

    tidy up unused functions

--- client/src/api/game.js
+++ client/src/api/game.js
@@ -10,28 +10,6 @@ async function createGame() {
   return response.json()
 }
 
-async function getGame(id) {
-  const response = await fetch(`/games/${id}`)
-
-  if (!response.ok) {
-    throw new Error('Game not found')
-  }
-
-  return response.json()
-}
-
-async function joinGame(id) {
-  const response = await fetch(`/games/${id}`)
-
-  if (!response.ok) {
-    throw new Error('Game not found')
-  }
-
-  return response.json()
-}
-
 export {
   createGame,
-  getGame,
-  joinGame,
 }
